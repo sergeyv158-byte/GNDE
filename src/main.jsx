140692cea222bda27cb2ab3f66415fb290c94d10
sergeyv158-byte <sergeyv158@hotmail.com>
2026-03-28 19:10:54 -0500

    Remove React imports and add Tailwind CSS
    Replaced React and component imports with Tailwind CSS directives.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import GuidedNDEWebsite from './GuidedNDEWebsite';
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><GuidedNDEWebsite /></React.StrictMode>);
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
